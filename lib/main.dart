16b3e3d5bb27dc42ebf6c2002331920056fadc04
rojoslavik <t6nejy00@students.oamk.fi>
2021-01-29 13:17:33 +0200

    Layout changes, refactoring, added bars for CPU and GPU usage.

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,6 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
@@ -18,19 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: Colors.black,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'System Manager'),
@@ -41,15 +28,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -59,108 +37,120 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
+  /*
   void _incrementCounter() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
-  }
+  } */
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+      /* appBar: AppBar(
         title: Text(widget.title),
-      ),
-      body: Center(
-        heightFactor: 300,
-        widthFactor: 400,
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Row(
+      ), */
+      body: _buildContent(),
+    );
+  }
 
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              constraints: BoxConstraints(minHeight: 150, minWidth: 150, maxHeight: 250, maxWidth: 300),
-              child: SfRadialGauge(axes: <RadialAxis> [
-                RadialAxis(
-                  minimum: 0,
-                  maximum: 115,
-                  showLabels: false,
-                  showTicks: false,
-                  axisLineStyle: AxisLineStyle(
-                    thickness: 0.1,
-                    cornerStyle: CornerStyle.bothCurve,
-                    color:  Color.fromARGB(30, 0, 169, 181),
-                    thicknessUnit: GaugeSizeUnit.factor,
-                  ),
-                  annotations: <GaugeAnnotation>[
-                    GaugeAnnotation(
-                      positionFactor: 0.05,
-                      angle: 90,
-                      widget: Text("CPU USAGE",
-                        style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)
+  Widget _buildContent() {
+    return Container(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            // color: Colors.yellowAccent,
+            constraints: BoxConstraints(
+                minHeight: 150.0,
+                minWidth: 150.0,
+                maxHeight: 400.0,
+                maxWidth: 300.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SfRadialGauge(
+                  axes: <RadialAxis>[
+                    RadialAxis(
+                      minimum: 0,
+                      maximum: 115,
+                      showLabels: false,
+                      showTicks: false,
+                      axisLineStyle: AxisLineStyle(
+                        thickness: 0.1,
+                        cornerStyle: CornerStyle.bothCurve,
+                        color: Color.fromARGB(30, 0, 169, 181),
+                        thicknessUnit: GaugeSizeUnit.factor,
                       ),
+                      annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                          positionFactor: 0.05,
+                          angle: 90,
+                          widget: Text("CPU USAGE",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold)),
+                        )
+                      ],
                     )
-                  ]
-                )
-              ]),
+                  ],
+                ),
+                FAProgressBar(
+                  backgroundColor: Colors.blue,
+                  currentValue: 0,
+                  maxValue: 100,
+                  displayText: "%",
+                ),
+              ],
             ),
-
-            Container(
-              constraints: BoxConstraints(minHeight: 150, minWidth: 150, maxHeight: 250, maxWidth: 300),
-              child: SfRadialGauge(axes: <RadialAxis> [
-                RadialAxis(
-                  minimum: 0,
-                  maximum: 115,
-                  showLabels: false,
-                  showTicks: false,
-                  axisLineStyle: AxisLineStyle(
-                    thickness: 0.1,
-                    cornerStyle: CornerStyle.bothCurve,
-                    color:  Color.fromARGB(30, 0, 169, 181),
-                    thicknessUnit: GaugeSizeUnit.factor,
-                  ),
-                  annotations: <GaugeAnnotation>[
-                    GaugeAnnotation(
-                      positionFactor: 0.05,
-                      angle: 90,
-                      widget: Text("GPU USAGE",
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+          ),
+          SizedBox(width: 159.2),
+          Container(
+            padding: EdgeInsets.only(left: 10.0, right: 10.0),
+            //color: Colors.red,
+            constraints: BoxConstraints(
+                minHeight: 150.0,
+                minWidth: 150.0,
+                maxHeight: 400.0,
+                maxWidth: 300.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SfRadialGauge(
+                  axes: <RadialAxis>[
+                    RadialAxis(
+                      minimum: 0,
+                      maximum: 115,
+                      showLabels: false,
+                      showTicks: false,
+                      axisLineStyle: AxisLineStyle(
+                        thickness: 0.1,
+                        cornerStyle: CornerStyle.bothCurve,
+                        color: Color.fromARGB(30, 0, 169, 181),
+                        thicknessUnit: GaugeSizeUnit.factor,
+                      ),
+                      annotations: <GaugeAnnotation>[
+                        GaugeAnnotation(
+                          positionFactor: 0.05,
+                          angle: 90,
+                          widget: Text("GPU USAGE",
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold)),
+                        )
+                      ],
                     )
-                ])
-              ]
-              ),
+                  ],
+                ),
+                FAProgressBar(
+                  backgroundColor: Colors.blue,
+                  currentValue: 0,
+                  maxValue: 100,
+                  displayText: "%",
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
